ab61363b79ad0e7161d230cbd387f97c8f0b4c4e
SungJinKang <hour30000@gmail.com>
2021-06-01 18:54:55 +0900

    add CullDistanceVolume struct

--- CullingModule/DistanceCulling/DistanceCulling.cpp
+++ CullingModule/DistanceCulling/DistanceCulling.cpp
@@ -1,2 +1,24 @@
 #include "DistanceCulling.h"
 
+#include <algorithm>
+
+void culling::DistanceCulling::AddCullDistanceVolume(culling::CullDistanceVolume cullDistanceVolume)
+{
+	this->mCullDistanceVolumes.push_back(cullDistanceVolume);
+	std::sort(this->mCullDistanceVolumes.begin(), this->mCullDistanceVolumes.end(),
+		[](const culling::CullDistanceVolume& A, const culling::CullDistanceVolume& B)
+		{
+			return A.mSize < B.mSize;
+		}
+	);
+}
+
+void culling::DistanceCulling::DeleteCullDistanceVolume(size_t index)
+{
+	if (index >= this->mCullDistanceVolumes.size())
+	{
+		return;
+	}
+
+	this->mCullDistanceVolumes.erase(this->mCullDistanceVolumes.begin() + index);
+}
--- CullingModule/DistanceCulling/DistanceCulling.h
+++ CullingModule/DistanceCulling/DistanceCulling.h
@@ -1,20 +1,55 @@
 #pragma once
 
+#include <vector>
+
 #include "../CullingModule.h"
 
 namespace culling
 {
+	struct CullDistanceVolume
+	{
+		/// <summary>
+		/// Volume size
+		/// </summary>
+		float mSize;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		float mCullDistance;
+	};
+
+	/// <summary>
+	/// https://docs.unrealengine.com/4.26/en-US/RenderingAndGraphics/VisibilityCulling/CullDistanceVolume/
+	/// 
+	/// Objects within the volume that have a size closest to CullDistanceVolume.mSize
+	/// will be culled from view 
+	/// when they are CullDistanceVolume.mCullDistance or more from the Camera.
+	/// </summary>
 	class DistanceCulling : CullingModule
 	{
+
 	private:
+
+		std::vector<culling::CullDistanceVolume> mCullDistanceVolumes;
 
 	public:
 
-		//TODO : EntityBlock의 mPositions 이거 그냥 사용하면된다.
+		// TODO : EntityBlock의 mPositions 이거 그냥 사용하면된다.
+		// 거리 비교할 때 sqrt하지말고 그냥 mCullDistance * mCullDistance로 비교하자
 		virtual void CullBlockEntityJob(EntityBlock* currentEntityBlock, size_t entityCountInBlock, size_t cameraIndex) final
 		{
+			if (mCullDistanceVolumes.size() == 0)
+			{
+				return;
+			}
+
 
 		}
+
+		void AddCullDistanceVolume(culling::CullDistanceVolume cullDistanceVolume);
+		void DeleteCullDistanceVolume(size_t index);
+
 	};
 }
 
