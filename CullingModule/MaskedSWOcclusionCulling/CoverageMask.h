588d6296a9497de73ad5ea19ecb34dd84e693350
SungJinKang <hour30000@gmail.com>
2021-06-08 14:20:30 +0900

    add CoverageMaskPair for preventing false sharing between threads

--- CullingModule/MaskedSWOcclusionCulling/CoverageMask.h
+++ CullingModule/MaskedSWOcclusionCulling/CoverageMask.h
@@ -7,7 +7,8 @@
 namespace culling
 {
 	/// <summary>
-	/// 16byte
+	/// 32byte
+	/// 
 	/// </summary>
 	struct CoverageMask
 	{
@@ -15,11 +16,36 @@ namespace culling
 		/// For using SIMD, variable should be aligned to 16 byte(AVX1) or 32 byte(AVX2)
 		/// </summary>
 #if TILE_WIDTH == 16
-		M128I mBits;
+		static_assert(false, "You need to check alignment");
+		M128I mBits; // 16byte
 #elif TILE_WIDTH == 32
-		M256I mBits;
+		M256I mBits; // 32byte
 #endif	 
 
+	};
+
+	static_assert(sizeof(CoverageMask) == 32);
+
+	/// <summary>
+	/// 
+	/// False sharing Safety CoverageMask
+	/// 
+	/// How this type is uesd.
+	/// 
+	/// In Masked SW Occlusion culling,
+	/// Multiple thread works on tile(32 * 8)
+	/// Size of CoverageMask is 256bit(32byte) 
+	/// Then Each Thread should works on different cache line ( False Sharing!!! )
+	/// 
+	///     64   ......   128   ......   192   .......  256    ......   320
+	///     CoverageMask1.CoverageMask2..CoverageMask3..CoverageMask4...
+	///		CachceLine1----------------->CachceLine2------------------->
+	///     Thread1--------------------->Thread2----------------------->
+	/// 
+	/// </summary>
+	struct CoverageMaskPair
+	{
+		alignas(64) CoverageMask CoverageMask[2];
 	};
 	
 }
