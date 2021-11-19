f8c8ec787de83db49fc7e3d925895009c20ff124
sungJinKangdev <hour30000@gmail.com>
2021-11-19 21:22:51 +0900

    update reflection data.

---
+++ EveryCulling.reflection.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Utility Header File ( Don't Edit this )
+// SourceFilePath : C:\Doom3FromScratch\Doom3\Source\Core\Graphics\Acceleration\LinearData_ViewFrustumCulling\EveryCulling.cpp
+
+
+#ifdef C__Doom3FromScratch_Doom3_Source_Core_Graphics_Acceleration_LinearData_ViewFrustumCulling_EveryCulling_reflection_h
+
+#error "C:\Doom3FromScratch\Doom3\Source\Core\Graphics\Acceleration\LinearData_ViewFrustumCulling\EveryCulling.reflection.h already included, missing '#pragma once' in C:\Doom3FromScratch\Doom3\Source\Core\Graphics\Acceleration\LinearData_ViewFrustumCulling\EveryCulling.reflection.h"
+
+#endif
+
+#define C__Doom3FromScratch_Doom3_Source_Core_Graphics_Acceleration_LinearData_ViewFrustumCulling_EveryCulling_reflection_h
+
+
+#include <type_traits>
+#include <cassert>
+
+
+//-------------------------------------------
+
+
