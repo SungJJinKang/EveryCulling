05cb93e1f534ffdead4f8775ee13c5392348b67f
sungJinKangdev <hour30000@gmail.com>
2021-12-21 04:45:10 +0900

    update reflection.h files

--- DataType/Math/Common.reflection.h
+++ DataType/Math/Common.reflection.h
@@ -4,13 +4,13 @@
 // SourceFilePath : C:\Doom3FromScratch\Doom3\Source\Core\Graphics\Acceleration\LinearData_ViewFrustumCulling\DataType\Math\Common.cpp
 
 
-#ifdef C__Doom3FromScratch_Doom3_Source_Core_Graphics_Acceleration_LinearData_ViewFrustumCulling_DataType_Math_Common_reflection_h
+#ifdef _Source_Core_Graphics_Acceleration_LinearData_ViewFrustumCulling_DataType_Math_Common_reflection_h
 
-#error "C:\Doom3FromScratch\Doom3\Source\Core\Graphics\Acceleration\LinearData_ViewFrustumCulling\DataType\Math\Common.reflection.h already included, missing '#pragma once' in C:\Doom3FromScratch\Doom3\Source\Core\Graphics\Acceleration\LinearData_ViewFrustumCulling\DataType\Math\Common.reflection.h"
+#error "_Source_Core_Graphics_Acceleration_LinearData_ViewFrustumCulling_DataType_Math_Common_reflection_h already included, missing '#pragma once' in _Source_Core_Graphics_Acceleration_LinearData_ViewFrustumCulling_DataType_Math_Common_reflection_h"
 
 #endif
 
-#define C__Doom3FromScratch_Doom3_Source_Core_Graphics_Acceleration_LinearData_ViewFrustumCulling_DataType_Math_Common_reflection_h
+#define _Source_Core_Graphics_Acceleration_LinearData_ViewFrustumCulling_DataType_Math_Common_reflection_h
 
 
 #include <type_traits>
