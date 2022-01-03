c68c3264b33c6134b0ce41d820a6784a7371dc71
SungJinKang <hour30000@gmail.com>
2022-01-03 17:17:25 +0900

    add clamp to screen space position before compute min, max overlapped tile origin point to prevent floating point buffer overflow when convert to interger

--- CullingModule/MaskedSWOcclusionCulling/Utility/depthBufferTileHelper.h
+++ CullingModule/MaskedSWOcclusionCulling/Utility/depthBufferTileHelper.h
@@ -3,6 +3,8 @@
 #include "../../../EveryCullingCore.h"
 #include "../SWDepthBuffer.h"
 
+#define FLOAT_OVERFLOW_CHECKER (float)2147483645.0f
+
 namespace culling
 {
 	namespace depthBufferTileHelper
@@ -25,20 +27,23 @@ namespace culling
 			SWDepthBuffer& depthBuffer
 		)
 		{
-			culling::M256I minScreenPixelX, minScreenPixelY, maxScreenPixelX, maxScreenPixelY;
-			
 			static const culling::M256I WIDTH_MASK = _mm256_set1_epi32(~(TILE_WIDTH - 1));
 			static const culling::M256I HEIGHT_MASK = _mm256_set1_epi32(~(TILE_HEIGHT - 1));
 
-			minScreenPixelX = _mm256_cvttps_epi32(_mm256_floor_ps(_mm256_min_ps(pointAScreenPixelX, _mm256_min_ps(pointBScreenPixelX, pointCScreenPixelX))));
-			minScreenPixelY = _mm256_cvttps_epi32(_mm256_floor_ps(_mm256_min_ps(pointAScreenPixelY, _mm256_min_ps(pointBScreenPixelY, pointCScreenPixelY))));
-			maxScreenPixelX = _mm256_cvttps_epi32(_mm256_ceil_ps(_mm256_max_ps(pointAScreenPixelX, _mm256_max_ps(pointBScreenPixelX, pointCScreenPixelX))));
-			maxScreenPixelY = _mm256_cvttps_epi32(_mm256_ceil_ps(_mm256_max_ps(pointAScreenPixelY, _mm256_max_ps(pointBScreenPixelY, pointCScreenPixelY))));
+			// to prevent overflow.
+			// when floating point is converted to integer
+			const culling::M256F minScreenPixelFloatX = _mm256_max_ps(_mm256_set1_ps(-FLOAT_OVERFLOW_CHECKER), _mm256_min_ps(_mm256_floor_ps(_mm256_min_ps(pointAScreenPixelX, _mm256_min_ps(pointBScreenPixelX, pointCScreenPixelX))), _mm256_set1_ps(FLOAT_OVERFLOW_CHECKER)));
+			const culling::M256F minScreenPixelFloatY = _mm256_max_ps(_mm256_set1_ps(-FLOAT_OVERFLOW_CHECKER), _mm256_min_ps(_mm256_floor_ps(_mm256_min_ps(pointAScreenPixelY, _mm256_min_ps(pointBScreenPixelY, pointCScreenPixelY))), _mm256_set1_ps(FLOAT_OVERFLOW_CHECKER)));
+			const culling::M256F maxScreenPixelFloatX = _mm256_max_ps(_mm256_set1_ps(-FLOAT_OVERFLOW_CHECKER), _mm256_min_ps(_mm256_ceil_ps(_mm256_max_ps(pointAScreenPixelX, _mm256_max_ps(pointBScreenPixelX, pointCScreenPixelX))), _mm256_set1_ps(FLOAT_OVERFLOW_CHECKER)));
+			const culling::M256F maxScreenPixelFloatY = _mm256_max_ps(_mm256_set1_ps(-FLOAT_OVERFLOW_CHECKER), _mm256_min_ps(_mm256_ceil_ps(_mm256_max_ps(pointAScreenPixelY, _mm256_max_ps(pointBScreenPixelY, pointCScreenPixelY))), _mm256_set1_ps(FLOAT_OVERFLOW_CHECKER)));
 
-			minScreenPixelX = _mm256_max_epi32(minScreenPixelX, _mm256_set1_epi32(0));
-			minScreenPixelY = _mm256_max_epi32(minScreenPixelY, _mm256_set1_epi32(0));
-			maxScreenPixelX = _mm256_max_epi32(maxScreenPixelX, _mm256_set1_epi32(0));
-			maxScreenPixelY = _mm256_max_epi32(maxScreenPixelY, _mm256_set1_epi32(0));
+
+			const culling::M256I minScreenPixelX = _mm256_cvttps_epi32(minScreenPixelFloatX);
+			const culling::M256I minScreenPixelY = _mm256_cvttps_epi32(minScreenPixelFloatY);
+			const culling::M256I maxScreenPixelX = _mm256_cvttps_epi32(maxScreenPixelFloatX);
+			const culling::M256I maxScreenPixelY = _mm256_cvttps_epi32(maxScreenPixelFloatY);
+			
+
 			// How "and" works?
 			// 0000 0000 0110 0011 <- 96 = 32 * 3 + 3
 			//        AND
