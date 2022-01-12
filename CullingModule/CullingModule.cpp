7f8d38b41bb80ae5d82379ee23c5ae6eee3af3c7
SungJinKang <hour30000@gmail.com>
2022-01-12 19:42:13 +0900

    add TODO to CullingModule::GetNextEntityBlock.

--- CullingModule/CullingModule.cpp
+++ CullingModule/CullingModule.cpp
@@ -4,6 +4,14 @@
 
 culling::EntityBlock* culling::CullingModule::GetNextEntityBlock(const size_t cameraIndex, const bool forceOrdering)
 {
+	// TODO : Implement Cache Friendly GetNextEntityBlock to prevent cache coherency.
+	//        After thread finished its blocks, steal other threads's block. 
+	// Ex)
+	// First Thread : 1 4 7 10  ....
+	// Second Thread : 2 5 8 11 ....
+	// Third Thread : 3 6 9 12 ....
+	//
+
 	const std::uint32_t currentEntityBlockIndex = mCullJobState.mCurrentCulledEntityBlockIndex[cameraIndex].fetch_add(1, forceOrdering == true ? std::memory_order_seq_cst : std::memory_order_relaxed);
 
 	const size_t entityBlockCount = mCullingSystem->GetActiveEntityBlockCount();
